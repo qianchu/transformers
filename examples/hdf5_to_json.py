261e19068534f4ec0a5f5e5d74ec806a328fa582
qianchu <hey_flora@126.com>
2020-09-10 19:33:30 +0100

    hdf5 removed

--- examples/hdf5_to_json.py
+++ examples/hdf5_to_json.py
@@ -3,6 +3,7 @@ if __name__=='__main__':
     import sys
     import h5py
     import numpy as np
+    import os
 
     hdf5_file=sys.argv[1]
 
@@ -15,6 +16,8 @@ if __name__=='__main__':
     f.close()
 
     np.save(output_npy,np_dict)
+    os.remove(hdf5_file)
+    print (hdf5_file, 'removed')
     # with open(output_json,'w') as f:
     #     json.dump(f_json,f)
 
