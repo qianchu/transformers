cf5663552c0f2ef9e8c3346128333155d5791c4a
qianchu <hey_flora@126.com>
2020-10-10 20:05:06 +0100

    token cat

--- examples/run_xlwic.py
+++ examples/run_xlwic.py
@@ -231,7 +231,7 @@ def train(args, train_dataset, model, tokenizer):
                 inputs["token_type_ids"] = (
                     batch[2] if args.model_type in ["bert"] else None
                 )  # XLM and DistilBERT don't use segment_ids
-            outputs = model(**inputs)
+            outputs = model(**inputs,token_ids=inputs['token_ids'])
             loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
 
             if args.n_gpu > 1:
