1122e5305a931e9193708154d6cd1f76a8f03de1
qianchu <hey_flora@126.com>
2020-10-13 17:18:54 +0100

    xlm token

--- src/transformers/modeling_xlm_roberta.py
+++ src/transformers/modeling_xlm_roberta.py
@@ -24,6 +24,7 @@ from .modeling_roberta import (
     RobertaForMaskedLM,
     RobertaForMultipleChoice,
     RobertaForSequenceClassification,
+    RobertaForSequenceTokenClassification
     RobertaForTokenClassification,
     RobertaModel,
 )
