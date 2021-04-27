b4d8ece6ed3f65637c6e701e174f9cf69e827ef2
qianchu <hey_flora@126.com>
2021-04-27 14:58:04 +0100

    deberta

--- src/transformers/tokenization_deberta.py
+++ src/transformers/tokenization_deberta.py
@@ -218,4 +218,4 @@ class DebertaTokenizer(GPT2Tokenizer):
         add_prefix_space = kwargs.pop("add_prefix_space", self.add_prefix_space)
         if (is_split_into_words or add_prefix_space) and (len(text) > 0 and not text[0].isspace()):
             text = " " + text
-        return (text, kwargs)
+        return text
